6a92e10151fdb937af26cbacf75fcc80c4191397
jacobjavier <jacobjavier95@gmail.com>
2023-10-12 22:30:06 -0600

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/authenticate', (req, res) => {
+    // Lógica de autenticación en tu backend Node.js
+    // Deberías realizar la autenticación aquí y devolver la respuesta.
+});
+
+module.exports = router;
