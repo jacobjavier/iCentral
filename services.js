f870d64d1350cdc6c6eac2aadd3ad0b0b6c58692
jacobjavier <jacobjavier95@gmail.com>
2023-10-12 22:30:27 -0600

    Create services.js

---
+++ services.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/getservices/:userId', (req, res) => {
+    // Lógica para obtener servicios en tu backend Node.js
+    // Deberías obtener los servicios del usuario con el userId proporcionado y devolver la respuesta.
+});
+
+module.exports = router;
