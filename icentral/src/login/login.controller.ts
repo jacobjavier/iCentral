bbde63dc4185c33f3ddac78c89332d98e7aa2281
jacobjavier <jacobjavier95@gmail.com>
2023-10-14 22:00:02 -0600

    new file:   icentral/src/login/login.controller.spec.ts 	new file:   icentral/src/login/login.controller.ts 	new file:   icentral/src/login/login.module.ts 	new file:   icentral/src/login/login.service.spec.ts 	new file:   icentral/src/login/login.service.ts

---
+++ icentral/src/login/login.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get } from '@nestjs/common';
+import axios from 'axios';
+
+@Controller('login')
+export class LoginController {
+    @Get('login')
+    async login() {
+      const data = {
+        username: 'client',
+        password: 'pa$$word',
+        expiration: 604800,
+        sliding: 0,
+        deviceName: 'My Phone Model',
+      };
+  
+      try {
+        const response = await axios.post('https://portal.icentral.com.mx/crm/api/v1.0/client-zone/login', data, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+  
+        console.log('Status:', response.status);
+        console.log('Headers:', response.headers);
+        console.log('Response:', response.data);
+  
+        return response.data; // Optionally return the response data to the client
+      } catch (error) {
+        // Handle error
+        console.error('Error:', error);
+        throw error;
+      }
+    }
+  
+}
